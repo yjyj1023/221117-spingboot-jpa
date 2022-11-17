01d04753bf555ffbb1a5a2f7ece750929665d6cc
yjyj1023 <lyj19961023@gmail.com>
2022-11-17 13:28:45 +0900

    User 수정

--- src/main/java/com/springbootjpa/domain/User.java
+++ src/main/java/com/springbootjpa/domain/User.java
@@ -15,15 +15,8 @@ import javax.persistence.Table;
 @Table(name = "user")
 public class User {
     @Id
-
     Long id;
     String username;
     String password;
-
-    public static UserResponse of(User user){
-        return new UserResponse(user.getId(),
-                user.getUsername(),
-                user.getPassword());
-    }
 
 }
